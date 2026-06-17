144489b3d31efac0e8a87ca4ce0cef92609ef552
visam <visamvenkatasai9550@gmail.com>
2026-06-17 20:33:15 +0530

    Changes in qdrant db file

--- backend/app/db/qdrent.py
+++ backend/app/db/qdrent.py
@@ -8,6 +8,7 @@ _qdrant_client = None
 def get_qdrant_client(path: str = "./qdrant_data"):
     global _qdrant_client
     if _qdrant_client is not None:
+        print("Nooooooooooooooooooooooooooo")
         return _qdrant_client
 
     qdrant_url = os.getenv("QDRANT_URL")
@@ -15,7 +16,9 @@ def get_qdrant_client(path: str = "./qdrant_data"):
 
     if qdrant_url:
         _qdrant_client = QdrantClient(url=qdrant_url, api_key=qdrant_api_key)
-    else:
-        _qdrant_client = QdrantClient(path=path, force_disable_check_same_thread=True)
+        print("...........................................")
+    # else:
+    #     print(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,")
+    #     _qdrant_client = QdrantClient(path=path, force_disable_check_same_thread=True)
 
     return _qdrant_client
